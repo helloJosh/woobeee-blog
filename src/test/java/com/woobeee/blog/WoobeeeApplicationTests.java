163d4dd8d5b6b0a05a329ba1a1349b765daadcf5
helloJosh <kimjoshua135@gmail.com>
2024-08-05 15:42:58 +0900

    feat: project started

---
+++ src/test/java/com/woobeee/blog/WoobeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woobeee.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoobeeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
