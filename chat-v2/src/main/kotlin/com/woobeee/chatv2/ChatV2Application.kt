abdc2d9b766d2fc0f7ca959c73f76d56a8829939
byungwookim-amberroad <byungwookim@amberroad.ai>
2025-10-23 16:34:34 +0900

    fix:chat kotlin webflux module added

---
+++ chat-v2/src/main/kotlin/com/woobeee/chatv2/ChatV2Application.kt
@@ -0,0 +1,11 @@
+package com.woobeee.chatv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatV2Application
+
+fun main(args: Array<String>) {
+    runApplication<ChatV2Application>(*args)
+}
---
+++ chat-v2/src/test/kotlin/com/woobeee/chatv2/ChatV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.woobeee.chatv2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatV2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
